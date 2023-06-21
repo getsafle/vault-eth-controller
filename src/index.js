dc0df492656e149cd0f3d70e2beff06867e4814c
SDargarh <sdargarh26@gmail.com>
2023-06-21 21:52:00 +0530

    import wallet standardised private key input

--- src/index.js
+++ src/index.js
@@ -233,7 +233,12 @@ class KeyringController extends EventEmitter {
 
   importWallet(_privateKey) {
     try {
+
+      if (_privateKey.startsWith('0x')) {
+        _privateKey = _privateKey.slice(2)
+      }
       const privateKey = Buffer.from(_privateKey, 'hex')
+      
       if (!ethUtil.isValidPrivate(privateKey))
         throw "Enter a valid private key"
 
